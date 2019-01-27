cb491b77e97bf35b837cd64b7cc22d1d46583c9f
root <johnj10j10@gmail.com>
2019-01-27 18:41:51 -0500

    Updated system folder to CodeIgniter 3.1.10

--- application/third_party/MX/Loader.php
+++ application/third_party/MX/Loader.php
@@ -297,7 +297,12 @@ class MX_Loader extends CI_Loader
 			$view = $_view;
 		}
 
-		return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
+		if (method_exists($this, '_ci_object_to_array'))
+		{
+				return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
+		} else {
+				return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_prepare_view_vars($vars), '_ci_return' => $return));
+		}
 	}
 
 	protected function &_ci_get_component($component)
